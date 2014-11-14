a179c1454d3bb582a5b6ca8b1873760f3680f1aa
Emin Bugra <eminbugrasaral@me.com>
2014-11-13 23:31:16 -0800

    Add alias for SentenceBuilder::Builder

--- lib/sentence_builder.rb
+++ lib/sentence_builder.rb
@@ -2,7 +2,12 @@ require "sentence_builder/builder"
 require "sentence_builder/version"
 
 module SentenceBuilder
-  def self.builder(nodes = [])
-    Builder.new(nodes)
+  class << self
+    # Alias for SentenceBuilder::Builder.new
+    #
+    # @return [SentenceBuilder::Builder]
+    def new(nodes = [])
+      Builder.new(nodes)
+    end
   end
 end
